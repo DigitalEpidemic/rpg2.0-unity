7d4757b0b4504aaebf85e113a3d8955728c7f935
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-09-13 23:01:30 -0400

    Added camera horizontal rotation, scroll to zoom and protection from wall clipping

---
+++ Assets/CameraCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour {
+
+    //[SerializeField] float minDistance = 1.0f;
+    //[SerializeField] float maxDistance = 4.0f;
+    //[SerializeField] float smooth = 10.0f;
+    //Vector3 dollyDir;
+    //[SerializeField] Vector3 dollyDirAdjusted;
+    //[SerializeField] float distance;
+
+    void Awake () {
+        //dollyDir = transform.localPosition.normalized;
+        //distance = transform.localPosition.magnitude;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.DrawRay(transform.position, Vector3.back * 10f);
+        ////Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.back, out hit, 10f)) {
+            float distance = Vector3.Distance(transform.position, hit.point);
+            //print(distance);
+            //transform.position = new Vector3();
+        }
+    }
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,16 +5,79 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     GameObject player;
+    float speed = 100.0f;
+
+    Vector3 hitLocation;
+
+    // Scrollwheel
+    [SerializeField] float minFov = 15f;
+    [SerializeField] float maxFov = 90f;
+    [SerializeField] float sensitivity = 10f;
+    float defaultFov;
+    float previousFov;
+    bool hasScrolled = false;
+
+    ProtectCameraFromWallClip protectCameraFromWallClip;
 
     // Use this for initialization
     void Start() {
+        defaultFov = Camera.main.fieldOfView;
         player = GameObject.FindGameObjectWithTag("Player");
-        //print (player.ToString ());
+        protectCameraFromWallClip = GetComponent<ProtectCameraFromWallClip>();
     }
 
     // Called after all Update functions have been called
     void LateUpdate() {
         transform.position = player.transform.position;
+
+        var cameraRotation = new Vector3(0f, Input.GetAxis("Horizontal"), 0f);
+        transform.Rotate(cameraRotation * speed * Time.deltaTime);
+
+        
+        PlayerZoom();
+    }
+
+    void PlayerZoom() {
+        float fov = Camera.main.fieldOfView;
+        if (!protectCameraFromWallClip.protecting) {
+            if ((Input.GetAxis("Mouse ScrollWheel") > 0f) || Input.GetAxis("Mouse ScrollWheel") < 0f) {
+                fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+                fov = Mathf.Clamp(fov, minFov, maxFov);
+                previousFov = fov;
+                hasScrolled = true;
+                Camera.main.fieldOfView = fov;
+            }
+        }
+
+        if (protectCameraFromWallClip.protecting) {
+            fov = defaultFov;
+            Camera.main.fieldOfView = fov;
+        }
+
+        if (!protectCameraFromWallClip.protecting) {
+            if (hasScrolled) {
+                fov = previousFov;
+                //Camera.main.fieldOfView = fov;
+                //Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, previousFov, Time.deltaTime*3f);
+                Camera.main.fieldOfView = Mathf.MoveTowards(Camera.main.fieldOfView, previousFov, Time.deltaTime*85f);
+            }
+        }
+
+        print("HasScrolled: " + hasScrolled);
+        
+        // TODO Fix camera FoV when turning away from wall if previously zoomed
+    }
+
+    void CompensateForWalls() {
+        var playerTransform = player.transform.position;
+        var cameraTransform = Camera.main.transform.position;
+
+        Debug.DrawLine(playerTransform, cameraTransform, Color.cyan);
+
+        RaycastHit wallHit;
+        if (Physics.Linecast(playerTransform, cameraTransform, out wallHit)) {
+            wallHit.transform.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, 0.2f);
+        }
     }
 
 } // CameraFollow
